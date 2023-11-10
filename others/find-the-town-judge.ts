553cc58e4aad87fb28389736871de30c215c62ac
savio04 <savio27.05.2001@gmail.com>
2023-11-10 18:04:06 -0300

    Feat: First problems

---
+++ others/find-the-town-judge.ts
@@ -0,0 +1,34 @@
+function findJudge(n: number, trust: number[][]): number {
+  if (trust.length === 0 && n === 1) return 1;
+
+  const countMap = new Map();
+  const visitors = new Set();
+
+  for (let index = 0; index < trust.length; index++) {
+    const visiteTrust = trust[index][0];
+    const currentTrust = trust[index][1];
+
+    if (countMap.has(currentTrust)) {
+      const currentItem = countMap.get(currentTrust);
+
+      countMap.set(currentTrust, currentItem + 1);
+    } else {
+      countMap.set(currentTrust, 1);
+    }
+
+    visitors.add(visiteTrust);
+  }
+
+  for (const [key, count] of countMap.entries()) {
+    if (count === n - 1 && !visitors.has(key)) return key;
+  }
+
+  return -1;
+}
+
+console.log(
+  findJudge(2, [
+    [1, 2],
+    [2, 1],
+  ])
+);
---
+++ strings/valid-parentheses.ts
@@ -0,0 +1,21 @@
+function isValid(s: string): boolean {
+  const stack: string[] = [];
+
+  for (const character of s) {
+    stack.unshift(character);
+
+    let removeFromStack = false;
+
+    if (stack[0] === ")" && stack[1] === "(") removeFromStack = true;
+
+    if (stack[0] === "]" && stack[1] === "[") removeFromStack = true;
+
+    if (stack[0] === "}" && stack[1] === "{") removeFromStack = true;
+
+    if (removeFromStack) stack.splice(0, 2);
+  }
+
+  return stack.length === 0;
+}
+
+console.log(isValid("()"));
