7467bb7dcfd29d039b7718e7d4f9872f1c4c9443
savio04 <savio27.05.2001@gmail.com>
2024-02-16 18:58:16 -0300

    Feat: Find minimun in rotated sorted array

---
+++ binary_search/find-minimum-in-rotated-sorted-array.ts
@@ -0,0 +1,20 @@
+/*
+ * Problem: https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/description/
+ * */
+
+function findMin(nums: number[]): number {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left < right) {
+    const middle = Math.trunc((left + right) / 2);
+
+    if (nums[middle] > nums[right]) {
+      left = middle + 1;
+    } else {
+      right = middle;
+    }
+  }
+
+  return nums[left];
+}
