a330d4846f620ae1d3fe01ff299c4afd669f2a53
savio04 <savio27.05.2001@gmail.com>
2024-03-01 17:43:06 -0300

    Feat: Invert binary tree

---
+++ trees/invert-binary-tree.ts
@@ -0,0 +1,27 @@
+/*
+ *Problem: https://leetcode.com/problems/invert-binary-tree/
+ * */
+
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+function invertTree(root: TreeNode | null): TreeNode | null {
+  if (root === null) return null;
+
+  const aux = root.left;
+  root.left = root.right;
+  root.right = aux;
+
+  invertTree(root.left);
+  invertTree(root.right);
+
+  return root;
+}
